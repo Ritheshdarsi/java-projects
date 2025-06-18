3cb38b55fa426edb999fc7b7e70614bfecd4460e
Ritheshdarsi <darsirithesh@gmail.com>
2025-06-18 11:37:17 +0530

    Create singlenumber.java

---
+++ singlenumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> seen=new HashSet<>();
+        for (int i:nums){
+            if (seen.contains(i)) {
+                seen.remove(i);}
+            else {
+                seen.add(i);
+            }
+
+        }
+        return seen.iterator().next();
+    }
+}
